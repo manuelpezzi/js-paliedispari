62bd7cf927e482b2e22c3a6d12823f96beab0aeb
manuelpezzi <pezzimanuel@gmail.com>
2025-01-15 12:35:31 +0100

    creazione funzione

--- palindromi/index.js
+++ palindromi/index.js
@@ -1,2 +1,14 @@
 //Palidroma
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma//
+/*
+ 1-chiedere all'utente la parola
+ 2-creare la funzione per verificare se la parola è palindroma
+ 3-verifica e stampa il risultato*/
+
+
+let parola = prompt("inserisci qui la tua parola senza usare maiuscole o numeri : (osso)")
+
+function parolaPalindroma(parola) {
+    let parolainvertita = parola.split("").reverse().join("");
+    return parola === parolainvertita;
+}
