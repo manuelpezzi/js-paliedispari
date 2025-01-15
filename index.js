f6b241182e0d68ecbedb5827cd21c589897a9e2a
manuelpezzi <pezzimanuel@gmail.com>
2025-01-15 12:55:13 +0100

    funzioni create

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 //Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.//
+function generaNumeroRandom() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+function pariODispari(somma) {
+    return somma % === 0 ? "pari" : "dispari";
+}
