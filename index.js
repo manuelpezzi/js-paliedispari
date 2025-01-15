7b5206c1307b513bc7f2fb1ffd95fc15f2661b3a
manuelpezzi <pezzimanuel@gmail.com>
2025-01-15 13:26:15 +0100

    modifica funzione pari o dispari+ inserimento console log di verica+condizioni per vedere il vincitore+fine repo

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ function generaNumeroRandom() {
     return Math.floor(Math.random() * 5) + 1;
 }
 function pariODispari(somma) {
-    return somma % === 0 ? "pari" : "dispari";
+    if (somma % 2 === 0) {
+        return 'pari';
+    } else {
+        return 'dispari';
+    }
 }
-function gioco() {
-    let pariOdispari = prompt("scegli pari o dispari").toLocaleLowerCase();
-    let numeroGiocatore = parseInt(prompt("inserisci un numnero da 1 a 5 :"));
 
-}
+let sceltaUtente = prompt("scegli pari o dispari").toLocaleLowerCase();
+let numeroGiocatore = parseInt(prompt("inserisci un numnero da 1 a 5 :"));
+
 let numeroDelCompuer = generaNumeroRandom();
 
 let somma = numeroGiocatore + numeroDelCompuer;
 
 let risultato = pariODispari(somma);
+
+console.log(`tu hai scelto ${sceltaUtente} e hai giocato il numero ${numeroGiocatore}`);
+console.log(`il computer ha giocato il numero ${numeroDelCompuer}`);
+console.log(`la somma dei due numeri è ${somma} che è ${risultato}.`);
+if (sceltaUtente === risultato) {
+    console.log("hai vinto campione");
+} else {
+    console.log("hai perso");
+}
